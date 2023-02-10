98ba12a2e9eb6e978d3faa004b1586b88be2e08d
tinagaran2210 <49159157+tinagaran2210@users.noreply.github.com>
2023-02-11 01:56:24 +0800

    Add files via upload

---
+++ movierecommend/src/main/java/com/movierecommend/movierecommend/UserService.java
@@ -0,0 +1,16 @@
+//package com.movierecommend.movierecommend;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UserService {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    public User save(User user) {
+//        return userRepository.save(user);
+//    }
+//}
+//
